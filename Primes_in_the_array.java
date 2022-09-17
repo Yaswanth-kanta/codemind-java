14e592e12fc128b73d8bde256c291aaefc00bfef
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-09-17 12:09:17 +0530

    import java.util.*; class prime {     public static boolean pri(int n)     {         if (n==1 || n==0)         {             return false;         }         for (int i=2;i<=(int)Math.sqrt(n);i++)         {             if (n%i==0)             {                 return false;             }         }         return true;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int si=sc.nextInt();         int [] arr=new int [si];         int c=0;         for (int i=0;i<si;i++)         {             arr[i]=sc.nextInt();             if (pri(arr[i]))             {                 if (pri(arr[i]))                 {                     c+=1;                 }             }         }         System.out.println(c);     } }

---
+++ Primes_in_the_array.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class prime
+{
+    public static boolean pri(int n)
+    {
+        if (n==1 || n==0)
+        {
+            return false;
+        }
+        for (int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if (n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int si=sc.nextInt();
+        int [] arr=new int [si];
+        int c=0;
+        for (int i=0;i<si;i++)
+        {
+            arr[i]=sc.nextInt();
+            if (pri(arr[i]))
+            {
+                if (pri(arr[i]))
+                {
+                    c+=1;
+                }
+            }
+        }
+        System.out.println(c);
+    }
+}
