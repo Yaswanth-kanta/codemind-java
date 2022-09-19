582bacca1402334f958e64f450a99e34096dec66
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-09-19 15:40:46 +0530

    import java.util.*; class perfect{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int s=0;         for (int i=1;i<n;i++){             if (n%i==0){                 s+=i;             }         }         if (s==n){             System.out.println("True");         }         else             System.out.println("False");     } }

---
+++ Perfect_Number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class perfect{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int s=0;
+        for (int i=1;i<n;i++){
+            if (n%i==0){
+                s+=i;
+            }
+        }
+        if (s==n){
+            System.out.println("True");
+        }
+        else
+            System.out.println("False");
+    }
+}
