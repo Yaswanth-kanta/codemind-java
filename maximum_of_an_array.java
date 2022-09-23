be37a19f83d35043f1a00c2546d53ec1577bb6f0
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-09-23 23:10:36 +0530

    import java.util.*; class sum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int [] arr=new int [n];         for (int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int c=-1;         for (int i=0;i<n;i++)         {             if (arr[i]>c)             {                 c=arr[i];             }         }         System.out.print(c);     } }

---
+++ maximum_of_an_array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int [n];
+        for (int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int c=-1;
+        for (int i=0;i<n;i++)
+        {
+            if (arr[i]>c)
+            {
+                c=arr[i];
+            }
+        }
+        System.out.print(c);
+    }
+}
