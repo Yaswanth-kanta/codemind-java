822fcd9d16e1c1648ce2ab850bdbfc7f8c5f744e
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-09-23 20:08:10 +0530

    import java.util.*; class sum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int [] arr=new int [n];         int sum=0;         for (int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             int c=0;             for(int j=0;j<n;j++)             {                 if(arr[i]==arr[j])                 {                     c++;                     if(c==1)                     {                         if(arr[i]%2==0)                         {                             sum+=arr[i];                         }                     }                     if(j > i)                         arr[j] = 0;                 }             }         }         System.out.println(sum);     } }

---
+++ unique_even_elements_sum.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int [n];
+        int sum=0;
+        for (int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            int c=0;
+            for(int j=0;j<n;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    c++;
+                    if(c==1)
+                    {
+                        if(arr[i]%2==0)
+                        {
+                            sum+=arr[i];
+                        }
+                    }
+                    if(j > i)
+                        arr[j] = 0;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
