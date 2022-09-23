015bce39be29d45486334d024a38193724f8921f
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-09-23 19:52:27 +0530

    import java.util.*; class sum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int [] arr=new int [n];         for (int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int a=sc.nextInt();         int b=sc.nextInt();         int c=0,max=0;         for (int j=0;j<n;j++)         {             if (arr[j]>=a && arr[j]<=b)             {                 if (arr[j]>max)                 {                     max=arr[j];                     c+=1;                 }             }         }         if (c==0)             System.out.print(-1);         else             System.out.print(max);     } }

---
+++ maximum_of_numbers_between_A_and_B.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int [n];
+        for (int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=0,max=0;
+        for (int j=0;j<n;j++)
+        {
+            if (arr[j]>=a && arr[j]<=b)
+            {
+                if (arr[j]>max)
+                {
+                    max=arr[j];
+                    c+=1;
+                }
+            }
+        }
+        if (c==0)
+            System.out.print(-1);
+        else    
+            System.out.print(max);
+    }
+}
