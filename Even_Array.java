fb37c7ac1877586b8bc999a4273e6cf38ad1af79
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-09-23 22:02:37 +0530

    import java.util.*; class sum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int [] arr=new int [n];         for (int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int c=0;         for (int i=0;i<n;i++)         {             if (arr[i]%2==0)                 c+=1;         }         if (c==n)             System.out.print("True");         else             System.out.print("False");     } }

---
+++ Even_Array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int [n];
+        for (int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int c=0;
+        for (int i=0;i<n;i++)
+        {
+            if (arr[i]%2==0)
+                c+=1;
+        }
+        if (c==n)
+            System.out.print("True");
+        else
+            System.out.print("False");
+    }
+}
