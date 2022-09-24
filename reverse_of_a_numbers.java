b7c828f1ad903db11e335f24bc753df8bc640585
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-09-24 07:38:13 +0530

    import java.util.*; class sum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int r,s=0;         while(n!=0)         {             r=n%10;             s=s*10+r;             n=n/10;         }         System.out.print(s);     } }

---
+++ reverse_of_a_numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int r,s=0;
+        while(n!=0)
+        {
+            r=n%10;
+            s=s*10+r;
+            n=n/10;
+        }
+        System.out.print(s);
+    }
+}
