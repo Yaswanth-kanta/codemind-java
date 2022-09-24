ccebc627f175ad6d427ad4aa6070045710534827
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-09-24 10:14:45 +0530

    import java.util.*; class sum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int m=sc.nextInt();         int n=sc.nextInt();         if(m%2==0 || n%2==0)             System.out.print("Player 1");         else             System.out.print("Player 2");     } }

---
+++ Predict_the_winner_in_Coin_Game.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        if(m%2==0 || n%2==0)
+            System.out.print("Player 1");
+        else
+            System.out.print("Player 2");
+    }
+}
