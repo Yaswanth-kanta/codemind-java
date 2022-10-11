6b0a3e6bb8e688e6ff2de6fb2074e01e54223618
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-10-12 02:51:49 +0530

    import java.util.*; class cloth {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int [] arr=new int [n];         for (int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         ArrayList<Integer> ar=new ArrayList<Integer>();         for (int i=0;i<n;i++)         {             ar.add(arr[i]);         }         HashSet<Integer> a=new HashSet<Integer>();         for (int i=0;i<n;i++)         {             a.add(arr[i]);         }         for (int ele:ar)         {             if (Collections.frequency(ar,ele)>(int)n/2)             {                 System.out.print(ele);                 break;             }         }     } }

---
+++ Majority_Element.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class cloth
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int [n];
+        for (int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        ArrayList<Integer> ar=new ArrayList<Integer>();
+        for (int i=0;i<n;i++)
+        {
+            ar.add(arr[i]);
+        }
+        HashSet<Integer> a=new HashSet<Integer>();
+        for (int i=0;i<n;i++)
+        {
+            a.add(arr[i]);
+        }
+        for (int ele:ar)
+        {
+            if (Collections.frequency(ar,ele)>(int)n/2)
+            {
+                System.out.print(ele);
+                break;
+            }
+        }
+    }
+}
