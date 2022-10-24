fa6c064913844289bf319ce1f0be5ca49e077aaa
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-10-24 20:27:00 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         System.out.print((n*(n-3))/2);     } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print((n*(n-3))/2);
+    }
+}
