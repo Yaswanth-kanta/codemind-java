0f6d895cc9bd5b6bb33938b9e1a4ab5c9d273f4d
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-10-27 10:53:10 +0530

    import java.util.*; class code {     public static void main(String args[])     {         Scanner sc=new Scanner (System.in);         int n=sc.nextInt();         int [] arr=new int [n];         for (int i=0;i<n;i++)             arr[i]=sc.nextInt();         int c=0;         for (int i=1;i<n-1;i++)         {             if (arr[i-1]%2!=0 &&arr[i]%2==0 && arr[i+1]%2!=0)                 c+=1;         }         System.out.print(c);     } }

---
+++ count_even_numbers_between_odd_numbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class code
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        int [] arr=new int [n];
+        for (int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        int c=0;
+        for (int i=1;i<n-1;i++)
+        {
+            if (arr[i-1]%2!=0 &&arr[i]%2==0 && arr[i+1]%2!=0)
+                c+=1;
+        }
+        System.out.print(c);
+    }
+}
