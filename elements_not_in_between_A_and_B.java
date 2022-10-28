25a3200c6cc9650cf7116f42c26d6068c69a3dbb
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-10-28 10:19:43 +0530

    import java.util.*; class minimum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b[]=new int[a];         for(int i=0;i<a;i++)             b[i]=sc.nextInt();         int c=sc.nextInt();         int d=sc.nextInt();         ArrayList<Integer> al=new ArrayList<Integer>();         int x=0;         for(int i:b)         {             if(i<c || i>d)             {                 x+=1;                 System.out.print(i+" ");             }         }         if(x==0)             System.out.print(-1);     } }

---
+++ elements_not_in_between_A_and_B.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class minimum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b[]=new int[a];
+        for(int i=0;i<a;i++)
+            b[i]=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        ArrayList<Integer> al=new ArrayList<Integer>();
+        int x=0;
+        for(int i:b)
+        {
+            if(i<c || i>d)
+            {
+                x+=1;
+                System.out.print(i+" ");
+            }
+        }
+        if(x==0)
+            System.out.print(-1);
+    }
+}
